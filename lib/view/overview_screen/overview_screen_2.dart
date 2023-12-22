48f695f75607530f413618c9847b73d71bc3df2e
amal-tech-dev <amalrofficial911@gmail.com>
2023-12-22 19:06:19 +0530

    added overview_screen_1

---
+++ lib/view/overview_screen/overview_screen_2.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class OverviewScreen2 extends StatelessWidget {
+  OverviewScreen2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
